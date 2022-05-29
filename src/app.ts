a972421a9e99b8c47f180cc0d05573bf94e60d12
Vinicius Gimenes <vinicius.pgimenes@gmail.com>
2022-05-29 19:06:49 -0300

    implementing api integrations

--- src/app.ts
+++ src/app.ts
@@ -2,28 +2,58 @@ import axios from 'axios';
 import cheerio from 'cheerio';
 import Cheerio = cheerio.Cheerio;
 
-const url = 'https://br.investing.com/crypto/dogecoin';
 const AxiosInstance = axios.create();
 
-interface Crypto {
-    name: string;
-    shortName: string;
-    extractionTime: number;
-    variation: string;
+interface CryptoExtraction {
+    extractedAt: number;
     value: number;
 }
 
-AxiosInstance.get(url)
-    .then(response => {
-        const html = response.data;
-        const $ = cheerio.load(html);
-        const crypto: Crypto = {} as Crypto;
-        const cryptoData: Cheerio = $('.top.bold.inlineblock');
-        const cryptoValue = cryptoData.find('#last_last').text();
-        const parsedCryptoValue = Number(cryptoValue.replace(',', '.'));
-        crypto.name = $('.float_lang_base_1.relativeAttr').text();
-        crypto.value = parsedCryptoValue;
-        crypto.extractionTime = Date.now();
-        console.log(crypto);
-    })
-    .catch(console.error);
+interface Crypto {
+    id: number;
+    name: string;
+    shortName: string;
+}
+
+interface CryptoResponse {
+    data: Crypto[]
+}
+
+const apiUrl = 'http://localhost:8000';
+
+async function getAllCryptos(): Promise<CryptoResponse> {
+    return AxiosInstance.get(`${apiUrl}/api/cryptos/`)
+}
+
+async function scrapeCryptoPage(crypto: Crypto): Promise<CryptoExtraction> {
+    const url = `https://br.investing.com/crypto/${crypto.name.toLowerCase().replace(' ', '-')}`;
+    console.log('url', url);
+    const response = await AxiosInstance.get(url);
+    const html = response.data;
+    const $ = cheerio.load(html);
+    const cryptoExtraction: CryptoExtraction = {} as CryptoExtraction;
+    const cryptoData: Cheerio = $('.top.bold.inlineblock');
+    const cryptoValue = cryptoData.find('#last_last').text();
+    cryptoExtraction.value = Number(cryptoValue.replace('.', '').replace(',', '.'));
+    cryptoExtraction.extractedAt = Date.now();
+    return cryptoExtraction;
+}
+
+async function updateCryptoValue(crypto: Crypto, cryptoExtraction: CryptoExtraction) {
+    AxiosInstance.post(`${apiUrl}/api/cryptos/${crypto.id}/variations`,
+        {
+            "value": cryptoExtraction.value,
+            "extractedAt": cryptoExtraction.extractedAt
+        })
+        .then(() => console.log(`${crypto.name}: updated`))
+        .catch(() => console.error(`${crypto.name}: error on update`));
+}
+
+setInterval(() => {
+    getAllCryptos()
+        .then(({ data }) => data.forEach(crypto =>
+            scrapeCryptoPage(crypto)
+                .then(cryptoExtraction =>
+                    updateCryptoValue(crypto, cryptoExtraction).then())
+                .catch(console.error)));
+}, 1500);
